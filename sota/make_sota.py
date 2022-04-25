6d9ad8031e405a24a8ff1a56fd40452bcce2e917
Jacarte <javierca@kth.se>
2022-04-25 19:13:58 +0200

    Complete iteration

--- sota/make_sota.py
+++ sota/make_sota.py
@@ -5,7 +5,7 @@ import os
 BLACKLIST = ['artificial_diversity', 'natural_diversity', 'static', 'dynamic', 'security', 'reliability', 'fault_tolerance', 'nversion', 'number_of_variants', 'architecture', 'intermixing']
 SIZES = {
     'architecture': 'p{3cm}',
-    'mean': 'p{5cm}',
+    'mean': 'p{6cm}',
     'security': 'l',
     'fault_tolerance': 'l'
 }
